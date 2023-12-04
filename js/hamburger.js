0a4745561c388897c6b32550b6ecd787aa53dcb4
florian.dussling <florian.dussling@udg.de>
2023-12-04 07:58:30 +0100

    code cleanup

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,30 +1,17 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-	showSlides((slideIndex += n));
+function hamburgerNavBar() {
+	const burger = document.getElementById('nav-hamburger-navbar');
+	if (burger.classList.contains('pos')) {
+		burger.classList.remove('pos');
+	} else {
+		burger.classList.add('pos');
+	}
 }
 
-function currentSlide(n) {
-	showSlides((slideIndex = n));
+let navToggle = document.querySelector('.nav-toggle');
+let bars = document.querySelectorAll('.bar');
+
+function toggleHamburger(e) {
+	bars.forEach((bar) => bar.classList.toggle('x'));
 }
 
-function showSlides(n) {
-	let i;
-	let slides = document.getElementsByClassName('slides');
-	let dots = document.getElementsByClassName('dot');
-	if (n > slides.length) {
-		slideIndex = 1;
-	}
-	if (n < 1) {
-		slideIndex = slides.length;
-	}
-	for (i = 0; i < slides.length; i++) {
-		slides[i].style.display = 'none';
-	}
-	for (i = 0; i < dots.length; i++) {
-		dots[i].className = dots[i].className.replace(' active', '');
-	}
-	slides[slideIndex - 1].style.display = 'block';
-	dots[slideIndex - 1].className += ' active';
-}
+navToggle.addEventListener('click', toggleHamburger);
